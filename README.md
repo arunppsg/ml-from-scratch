fd8c98a9b9400b2218546cbfb1b12ca81cd76456
Arun <arun.palaniappan1999@gmail.com>
2022-01-06 19:13:53 +0530

    perceptron with batch updates

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This is a practice repo which I am building for learning ML
 algorithms in depth.
+
+## Algorithms Implemented
+- Logistic Regression
+- Perceptron
--- perceptron.py
+++ perceptron.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
 
 """
-Feed-forward neural networks
+Perceptron
 """
-
-# TODO Enable batch mode
 
 import pandas as pd
 import numpy as np
 
 def mean_squared_error_loss(y_pred, y_true):
-    return (y_pred - y_true) ** 2 
+    return np.mean((y_pred - y_true) ** 2)
 
-def fit(X, y, epoches=50):
+def fit(X, y, epoches=50, batch_size=50):
     W = np.random.uniform(-1, 1, 4)
     eta = 0.0001
     losses = []
+    n_batches = len(X) // batch_size
     for epoch in range(epoches):
         loss = 0
-        for i in range(0, len(X)):
-            y_pred = np.sum(W * X[i])
-            y_true = y[i]
+        for i in range(0, n_batches):
+            x = X[i*50:(i+1)*50]
+            y_true = y[i*50:(i+1)*50]
+            y_pred = np.sum(W * x, axis=1)
+            y_pred = y_pred[:,np.newaxis]
+
             loss += mean_squared_error_loss(y_pred, y_true)
-            gradient = 2 * (y_pred - y_true) * X[i]
+            gradient = np.mean(2 * (y_pred - y_true) * x, axis=0)
             W = W - eta * gradient
         losses.append(loss)
     return W, losses
 
 if __name__ == "__main__":
-    df = pd.read_csv('../data/regression_data.csv')
+    df = pd.read_csv('data/regression_data.csv')
     X = df[['x1', 'x2', 'x3']].values
-    y = df['y'].values
+    y = df[['y']].values
     bias = np.ones((len(X), 1))
     X = np.append(X, bias, axis=1)
     w, losses = fit(X, y)
